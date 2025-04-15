12ab9313802875b55087d8a9550ceb1a93defae9
Jefferson S. Motta <55891584+jssmotta@users.noreply.github.com>
2025-04-15 00:26:48 -0300

    views

--- sql-init/AgendaRelatorio.sql
+++ sql-init/AgendaRelatorio.sql
@@ -1,2 +1,9 @@
-CREATE OR ALTER VIEW [dbo].[AgendaRelatorio] AS SELECT qa.*, vp.* FROM [dbo].[view_QAgenda] qa JOIN agenda a on a.ageCodigo = qa.vqaCodigo LEFT JOIN [dbo].[view_QProcessos] vp
+CREATE OR ALTER VIEW [dbo].[AgendaRelatorio] AS SELECT qa.*, 
+
+vp.advNome as xxxNomeAdvogado,
+vp.forNome as xxxNomeForo,
+vp.jusNome as xxxNomeJustica,
+vp.areDescricao as xxxNomeArea
+
+FROM [dbo].[view_QAgenda] qa JOIN agenda a on a.ageCodigo = qa.vqaCodigo LEFT JOIN [dbo].[view_QProcessos] vp
   on a.ageIDInsProcesso = vp.insCodigo  
